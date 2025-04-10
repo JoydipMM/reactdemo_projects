9299187a941d6932ae837922641028704c775bad
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2025-04-10 11:52:20 +0530

    added

---
+++ nextjs/travscapeglobal-ts/lib/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+let isConnected = false;
+
+export const connectDB = async () => {
+  if (isConnected) {
+    console.log("✅ MongoDB already connected");
+    return;
+  }
+
+  try {
+    const dbConnection = await mongoose.connect(process.env.MONGODB_CONNECTION, {
+      dbName: "tour_DB",
+    });
+
+    isConnected = true;
+    console.log("✅ MongoDB connected:", dbConnection.connection.host);
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+    throw error;
+  }
+};
---
+++ nextjs/travscapeglobal-ts/lib/models/tour.Model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const tourSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    thumbimage:{
+        type:String,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now() 
+    }
+}, { timestamps: true })
+
+
+const ToursModel = mongoose.models.tour ||  mongoose.model("tour", tourSchema);
+
+export default ToursModel;
+
