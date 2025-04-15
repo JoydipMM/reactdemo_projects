0e479f538e961df26f07b4a9dd1e3767196d6120
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2025-04-15 11:05:51 +0530

    added

--- nextjs/travscapeglobal-ts/app/admin/crud/tour/wirte.tsx
+++ nextjs/travscapeglobal-ts/app/admin/crud/tour/wirte.tsx
@@ -11,29 +11,29 @@ interface TourData {
 export const createNewTour = async ({ data, thumbImage }: TourData) => {
 
     if(!data?.title){
-                throw new Error("Title is required")
-            }
-            if(!data?.description){
-                throw new Error("description is required")
-            }
-            if(!data?.thumbImage){
-                throw new Error("Thumb Image is required")
-            }
-          //console.log("Form data ---- ", data);
-          const imageRef = ref(storage, `uploads/${thumbImage}`);
-          await uploadBytes(imageRef, data?.thumbImage);
-          const thumbURL = getDownloadURL(imageRef);
-    
-          //console.log("firebase Image URL ---- ", await thumbURL);
-    
-          const formData = new FormData();
-          formData.append('title', data.title);
-          formData.append('description', data.description);
-          formData.append('thumbimage', await thumbURL);
-    
-          //console.log(formData);
-          const  respons = await axios.post("http://localhost:3000/api/tour", formData);
+        throw new Error("Title is required")
+    }
+    if(!data?.description){
+        throw new Error("description is required")
+    }
+    if(!data?.thumbImage){
+        throw new Error("Thumb Image is required")
+    }
+    //console.log("Form data ---- ", data);
+    const imageRef = ref(storage, `uploads/${thumbImage}`);
+    await uploadBytes(imageRef, data?.thumbImage);
+    const thumbURL = getDownloadURL(imageRef);
 
-          return respons;
+    //console.log("firebase Image URL ---- ", await thumbURL);
+
+    const formData = new FormData();
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+    formData.append('thumbimage', await thumbURL);
+
+    //console.log(formData);
+    const  respons = await axios.post("http://localhost:3000/api/tour", formData);
+
+    return respons;
 
 }
