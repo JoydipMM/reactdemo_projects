2955824f2a32dd6e3661856ff180ca2b4aa32318
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2025-04-27 15:50:09 +0530

    added

--- nodejs/hitesh-nodejs/node-structure/src/models/user.model.js
+++ nodejs/hitesh-nodejs/node-structure/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema({
     userName:{
@@ -43,5 +45,43 @@ const userSchema = new Schema({
        type: String 
     }
 }, { timestamps:true });
+
+// password encrypt before save data
+userSchema.pre("save", async function (req, res, next) {
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10);
+    next()
+})
+
+// custom methods for deceypt password
+userSchema.methods.isPasswordCorrect = async function
+(password){
+    return await bcrypt.compare(password, this.password) 
+    // result true / false
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    userName: this.userName,
+    fullName: this.fullName
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id: this._id,
+        }, 
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
