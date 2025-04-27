be89279ba7d543b5f72bea1c18c5435e2322f085
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2025-04-27 22:20:51 +0530

    added

--- nodejs/hitesh-nodejs/node-structure/src/app.js
+++ nodejs/hitesh-nodejs/node-structure/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // express configuration
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
-app.use(express.json({ limit:"16bk" }))
+app.use(express.json({ limit:"16kb" }))
 app.use(express.urlencoded({ extended:true,  limit: "16kb" }))
 app.use(cookieParser())
 
--- nodejs/hitesh-nodejs/node-structure/src/controllers/user.controller.js
+++ nodejs/hitesh-nodejs/node-structure/src/controllers/user.controller.js
@@ -2,9 +2,29 @@ import { asyncHandler } from "../utils/asyncHandeller.js";
 
 
 const registerUser = asyncHandler( async (req, res) =>{
-    res.status(200).json({
-        message:"ok"
-    })
+    
+    // test code ---------------------------------------------------
+    // res.status(200).json({
+    //     message:"ok"
+    // })
+
+
+    // get user details from frontend ==================================
+    const { email, userName, password } = req.body;
+    console.log("email", email);
+    // validation - 1. not empty =======================================
+    // check if user already exists: username, email  ==================
+    // check for images, check for avater  =============================
+    // upload images in cloudimary, avater uploaded check  =============
+    // create user object - create entry in db  ========================
+    // remove password and refresh token from response  ================
+    // check for user creation  ========================================
+    // return response  ================================================
+
+
+
+
+    
 })
 
 
