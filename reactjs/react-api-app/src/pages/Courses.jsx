cd70c3ad70e56ce50b3d4f49faf7ae49ddf5d26e
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2025-09-07 00:17:04 +0530

    added

--- reactjs/react-api-app/src/pages/Courses.jsx
+++ reactjs/react-api-app/src/pages/Courses.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import GridRow from '../components/GridRow/GridRow'
 import CourseCard from '../components/CourseCard/CourseCard'
 import {courses} from "../data/StaticData";
 
 const Courses = () => {
+
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  const sortedCourses = [...courses].sort((a, b) => {
+    return sortOrder === "asc"
+      ? a.title.localeCompare(b.title)
+      : b.title.localeCompare(a.title);
+  });
   return (
     <>
-      <h1 className='text-3xl font-bold text-purple-700 mb-6'>Course</h1>
+    <div className='flex justify-between items-center mb-6'>
+      <h1 className='text-3xl font-bold text-purple-700'>Course</h1>
+
+      <select
+        className="border-none py-2 px-4 rounded-2xl bg-gray-200 text-black text-xs"
+        value={sortOrder}
+        onChange={(e) => setSortOrder(e.target.value)}
+      >
+        <option value="asc">Sort A → Z</option>
+        <option value="desc">Sort Z → A</option>
+      </select>
+    </div>
 
       <GridRow gridcol='lg:grid-cols-3'>
-        {courses.map((item) => <CourseCard key={item.id} data={item}/>)}
+        {sortedCourses.map((item) => <CourseCard key={item.id} data={item}/>)}
       </GridRow>
 
     </>
--- reactjs/react-api-app/src/pages/Team.jsx
+++ reactjs/react-api-app/src/pages/Team.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState} from 'react';
 import GridRow from '../components/GridRow/GridRow'
 import TeamCard from '../components/TeamCard/TeamCard'
 import {team} from "../data/StaticData";
 
 const Team = () => {
+  const [sortOrder, setSortOrder] = useState("asc");
+  
+  const sortedTeam = [...team].sort((a, b) => {
+    return sortOrder === "asc"
+      ? a.name.localeCompare(b.name)
+      : b.name.localeCompare(a.name);
+  });
   return (
     <>
-      <h1 className='text-3xl font-bold text-purple-700 mb-6'>Team</h1>
+
+      <div className='flex justify-between items-center mb-6'>
+        <h1 className='text-3xl font-bold text-purple-700'>Team</h1>
+
+        <select
+          className="border-none py-2 px-4 rounded-2xl bg-gray-200 text-black text-xs"
+          value={sortOrder}
+          onChange={(e) => setSortOrder(e.target.value)}
+        >
+          <option value="asc">Sort A → Z</option>
+          <option value="desc">Sort Z → A</option>
+        </select>
+      </div>
       <GridRow gridcol='lg:grid-cols-3'>
-        {team.map((item) => <TeamCard key={item.id} data={item}/>)}
+        {/* {team.map((item) => <TeamCard key={item.id} data={item}/>)} */}
+        {sortedTeam.map((item) => <TeamCard key={item.id} data={item}/>)}
         
       </GridRow>
     </>
