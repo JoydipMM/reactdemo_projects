dacce9b567cdfe103d15e219e184b99af596f57c
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2025-09-06 23:53:30 +0530

    added

--- reactjs/react-api-app/src/components/UserProfile/UserProfile.jsx
+++ reactjs/react-api-app/src/components/UserProfile/UserProfile.jsx
@@ -17,11 +17,29 @@ const UserProfile = ({data}) => {
         <div className='flex gap-6'>
             <UserAvater avatar={avatar}/>
             <div className='flex-1'>
-                <GridRow className={'card pt-9'} gridcol="grid-cols-2">
+                <GridRow className={'card pt-9 mb-3 gap-2'} gridcol="grid-cols-2">
                     <LavelTag position='left' lavel={id} title="ID" />
-                    <KeyValueCard data={preferences} />
                     <div>
-                        <h3 className='text-xl font-bold text-cyan-900 mb-2'>Notification</h3>
+                        <div className="flex-1 text-2xl font-bold text-purple-700">{name}</div>
+                    </div>
+                    <div className="flex">
+                        <label className="text-[14px] text-stone-500">E-mail :</label>
+                        <div className="flex-1 text-[14px] px-2.5">{email}</div>
+                    </div>
+                    <div className="flex">
+                        <label className="text-[14px] text-stone-500">Location :</label>
+                        <div className="flex-1 text-[14px] px-2.5">{location}</div>
+                    </div>
+                    <div className="flex">
+                        <label className="text-[14px] text-stone-500">Joined :</label>
+                        <div className="flex-1 text-[14px] px-2.5">{location}</div>
+                    </div>
+                    <KeyValueCard data={preferences} />
+                </GridRow>
+
+                <GridRow className={'card'} gridcol="grid-cols-2">
+                    <div>
+                        <h3 className='text-lg font-bold text-cyan-900 mb-2'>Notification</h3>
                         <GridRow gridcol="grid-cols-4">
                             <KeyValueCard data={preferences.notifications} />
                         </GridRow>
