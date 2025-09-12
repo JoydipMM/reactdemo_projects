eea26e574e63693082989eda97f2fa23f45279c3
joydip <joydip@matrixnmedia.com>
2025-09-12 20:02:02 +0530

    added

---
+++ reactjs/react-api-app/src/components/NormalForm.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+const NormalForm = () => {
+
+    const [formData, setFormData] = useState({
+        name:"",
+        email:""
+    })
+
+    const changeEvent = (e) => {
+        const { name, value } = e.target;
+    }
+
+
+
+
+  const handleSubmit = (e) =>{  
+    e.preventDefault();
+    console.log("clicked");
+    
+  }  
+  return (
+    <div className={`card`}>
+        <div>
+            <h3 className="text-2xl font-bold text-cyan-900 mb-6">Simple Form</h3>
+            {/* <label className="text-xs text-gray-600 p-0 mb-0">Tagline: Practice. Build. Repeat.</label> */}
+        </div>
+
+        <form onSubmit={handleSubmit}>
+            <div className='w-full relative flex flex-col gap-y-5'>
+                <div className='w-full relative flex flex-col gap-y-2'>
+                    <label className='px-2.5'>User Name</label>
+                    <input 
+                    type='text' 
+                    className='formfield' 
+                    placeholder='Full Name' 
+                    name="username" 
+                    onChange={changeEvent} 
+                    value={formData.name} 
+                    />
+                </div>
+                <div className='w-full relative flex flex-col gap-y-2'>
+                    <label className='px-2.5'>User Email</label>
+                    <input
+                    type='email'
+                    className='formfield'
+                    placeholder='Email'
+                    name="useremail"
+                    value={formData.email}
+                    />
+                </div>
+                <div className='w-full relative flex gap-x-2 justify-end'>
+                    <button type='reset' className='py-1.5 px-3.5 bg-red-700 hover:bg-black rounded-2xl text-amber-50 flex items-center justify-center cursor-pointer'>Reset</button>
+                    <button type='submit' className='py-1.5 px-3.5 bg-blue-700 hover:bg-black rounded-2xl text-amber-50 flex items-center justify-center cursor-pointer'>Submit</button>
+                </div>
+            </div>
+        </form>
+
+
+    </div>
+  )
+}
+
+export default NormalForm
--- reactjs/react-api-app/src/pages/Home.jsx
+++ reactjs/react-api-app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { siteInfo } from "../data/StaticData";
 import GridRow from '../components/GridRow/GridRow';
 import SiteInfo from '../components/SiteInfo/SiteInfo';
+import NormalForm from '../components/NormalForm';
 
 const Home = () => {
 
@@ -9,8 +10,11 @@ const Home = () => {
     <>
     <h1 className='text-3xl font-bold text-purple-700 mb-6'>Welcome to React Mini App</h1>
 
-      <GridRow>
-        <SiteInfo name={siteInfo.name} tagline={siteInfo.tagline} stats={siteInfo.stats} links={siteInfo.links} />
+      <GridRow gridcol="grid-cols-2">
+        <GridRow>
+          <SiteInfo name={siteInfo.name} tagline={siteInfo.tagline} stats={siteInfo.stats} links={siteInfo.links} />
+        </GridRow>
+        <NormalForm/>
       </GridRow>
     </>
   )
