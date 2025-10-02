ee00db2820ae321197eab5af02dc96d03748321a
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2025-10-02 13:43:53 +0530

    added

---
+++ nodejs/nodejs-api-with-JWT-demo/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    useremail:{
+        type: String,
+        require: true,
+        unique:true,
+    },
+    userpassword:{
+        type: String,
+        require: true,
+    },
+    userphone:{
+        type: Number,
+        require: true,
+        unique:true,
+    },
+    gender:{
+        type: String,
+        enum: ["Male", "Female"],
+        require: true,
+    },
+    useravater:{
+        type:String,
+    }
+
+}, { timestamps: true });
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
