d108b680db52660cf5c07ca4ce416afec69f39ef
joydip <joydip@matrixnmedia.com>
2025-12-03 17:13:19 +0530

    added

---
+++ reactjs/react-redux-basic/src/components/Counter.jsx
@@ -0,0 +1,8 @@
+export default function Counter() {
+    return (
+        <div>
+            <h1>Counters</h1>
+            
+        </div>
+    );
+}
--- reactjs/react-redux-basic/src/redux/store.js
+++ reactjs/react-redux-basic/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 //import todoReducer from "./todoSlice";
 import { todoSlice } from "./todoSlice";
+import { counterSlice } from "./counterSlice";
 
 export const store = configureStore({
-   // reducer: todoReducer
-   reducer: todoSlice.reducer
+   //reducer: todoReducer
+   reducer: todoSlice.reducer,
+   //reducer: { todos: todoSlice.reducer, counter: counterSlice.reducer }
 })
