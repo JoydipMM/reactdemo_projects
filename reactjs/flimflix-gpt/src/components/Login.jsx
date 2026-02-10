051004f39300af8486a0a6978a960b1f6c4bc0ad
joydip <joydip@matrixnmedia.com>
2026-02-10 19:32:55 +0530

    added

--- reactjs/flimflix-gpt/src/components/Login.jsx
+++ reactjs/flimflix-gpt/src/components/Login.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Header from './Header'
 import { Link } from 'react-router-dom'
+import { checkValidData } from '../utils/validate'
 
 const Login = () => {
     const [isSigninForm, setIsSigninForm] = useState(true);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const fullnameRef = useRef(null);
 
     const toggleSignInForm = () => {
         setIsSigninForm((prev) => !prev);
     }
+
+
+    const handleButtonClick = (e) => {
+        e.preventDefault();
+        //console.log(emailRef.current.value);
+        //console.log(passwordRef.current.value);
+        const validateMsg = checkValidData(emailRef.current.value, passwordRef.current.value);
+        console.log(validateMsg);
+        setErrorMsg(validateMsg);
+    }
+
+
   return (
     <div>
         <Header/>
@@ -18,19 +36,20 @@ const Login = () => {
             <form>
                 {!isSigninForm && <div>
                     <label htmlFor="fullname">Full Name</label>
-                    <input type="password" name="fullname" id="fullname" className='form_field' placeholder='Fullname' />
+                    <input ref={fullnameRef} type="password" name="fullname" id="fullname" className='form_field' placeholder='Fullname' />
                 </div> }
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" className='form_field' placeholder='Email' />
+                    <input ref={emailRef} type="email" name="email" id="email" className='form_field' placeholder='Email' />
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" id="password" className='form_field' placeholder='Password' />
+                    <input ref={passwordRef} type="password" name="password" id="password" className='form_field' placeholder='Password' />
                 </div>
                 <div>
-                    <button type="submit" className='submit_btn login_btn'>{isSigninForm ? "Sign In" : "Sign Up"}</button>
+                    <button type="submit" className='submit_btn login_btn' onClick={handleButtonClick}>{isSigninForm ? "Sign In" : "Sign Up"}</button>
                 </div>
+                <p style={{color:"red"}}>{errorMsg}</p>
                 {isSigninForm ? 
                 <div>are you now? <button className='signup_text_btn' onClick={toggleSignInForm}>Signup</button></div> :
                 <div>already have an account? <button className='signup_text_btn' onClick={toggleSignInForm}>Signin</button></div>
