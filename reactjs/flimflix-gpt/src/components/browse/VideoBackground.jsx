8664115d3b4b099cf243b5227d1ab77f48e9dcd2
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-02-14 00:38:57 +0530

    convert movie trailer to custom hook

--- reactjs/flimflix-gpt/src/components/browse/VideoBackground.jsx
+++ reactjs/flimflix-gpt/src/components/browse/VideoBackground.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import YouTube from "react-youtube";
-import { TMDB_API_OPTION } from '../../utils/constants';
 import { useDispatch, useSelector } from 'react-redux';
-import { addTrailer, updateCurrentBrowseMovieIndex } from '../../utils/moviesSlice';
+import { updateCurrentBrowseMovieIndex } from '../../utils/moviesSlice';
+import useGetBrowseTrailers from '../../hooks/useGetBrowseTrailers';
+
+
 const VideoBackground = ({movieID, backdrop, poster}) => {
 
     const dispatch = useDispatch();
@@ -17,31 +19,13 @@ const VideoBackground = ({movieID, backdrop, poster}) => {
     const trailers = useSelector((store) => store.movies?.trailer);
     const [ currentIndex, setCurrentIndex ] = useState(useSelector((store) => store.movies?.currentBrowseMovieIndex));
 
-    console.log("current index:", currentIndex);
+    
+    useGetBrowseTrailers(movieID);
     
 
-    const movieVideoApi = async () => {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/${movieID}/videos`, TMDB_API_OPTION);
-        //console.log(response);
-        const movieVideoData = await response.json();
-        //console.log(movieVideoData);
-
-        //const videosFilter = movieVideoData.results.filter((item) => item.type === "Trailer");
-        //console.log(videosFilter);
-        // const trailer =  videosFilter.length ? videosFilter[0]?.key : movieVideoData.results[0]?.key;
-        // console.log(trailer);
-        // setTrailerID(trailer);
-
-        const trailerVideos = movieVideoData.results.filter((item) => item.type === "Trailer" && item.site === "YouTube");
-
-        //setTrailers(trailerVideos[0]);
-        dispatch(addTrailer(trailerVideos[0]));
-
-    }
-
-    useEffect(() => {
-        movieVideoApi();
-    }, [movieID]);
+    // useEffect(() => {
+    //     movieVideoApi(movieID);
+    // }, [movieID]);
 
     // video end event
 
@@ -59,12 +43,6 @@ const VideoBackground = ({movieID, backdrop, poster}) => {
 
 
     const handleVideoEnd = () => {
-        /*setCurrentIndex((prev) => prev=prev+1);
-
-        console.log(currentIndex);
-        dispatch(updateCurrentBrowseMovieIndex(currentIndex));
-        return currentIndex;*/
-
         setCurrentIndex((prev) => {
             const nextIndex = prev + 1;
             dispatch(updateCurrentBrowseMovieIndex(nextIndex));
---
+++ reactjs/flimflix-gpt/src/hooks/useGetBrowseTrailers.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { TMDB_API_OPTION } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailer } from "../utils/moviesSlice";
+
+const useGetBrowseTrailers = (movieID) => {
+  const dispatch = useDispatch();
+  const movieVideoApi = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieID}/videos`,
+      TMDB_API_OPTION,
+    );
+    const movieVideoData = await response.json();
+    //console.log(movieVideoData);
+
+    //const videosFilter = movieVideoData.results.filter((item) => item.type === "Trailer");
+    //console.log(videosFilter);
+    // const trailer =  videosFilter.length ? videosFilter[0]?.key : movieVideoData.results[0]?.key;
+    // console.log(trailer);
+    // setTrailerID(trailer);
+
+    const trailerVideos = movieVideoData.results.filter(
+      (item) => item.type === "Trailer" && item.site === "YouTube",
+    );
+
+    //setTrailers(trailerVideos[0]);
+    dispatch(addTrailer(trailerVideos[0]));
+  };
+
+  useEffect(() => {
+    movieVideoApi(movieID);
+  }, [movieID]);
+  
+};
+
+export default useGetBrowseTrailers;
