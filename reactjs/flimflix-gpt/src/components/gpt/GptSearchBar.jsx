53577564b62587cf7b180cbf382355e923ef2b3b
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-02-15 00:29:18 +0530

    gpt search added

--- reactjs/flimflix-gpt/src/components/gpt/GptSearchBar.jsx
+++ reactjs/flimflix-gpt/src/components/gpt/GptSearchBar.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import openai from '../../utils/openAi';
+import { useDispatch } from 'react-redux';
+import { lastGptSearch } from '../../utils/gptSlice';
+import { API_BASE_URL, TMDB_API_OPTION } from '../../utils/constants';
 
 const GptSearchBar = () => {
-  return (
-    <div>
-      <form>
-        <input type="text" placeholder="Search..." />
-        <button type="submit">Search</button>
-      </form>
-    </div>
-  )
+
+    const dispatch = useDispatch();
+
+    const inputRef = useRef(null);
+
+    const findSearchMovies = async (movieName) => {    
+        const response = await fetch(`${API_BASE_URL}/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`, TMDB_API_OPTION);
+        const data = await response.json();
+        return data.results;
+    }
+
+
+    const searchEvent = async (e) => {
+        e.preventDefault()
+        const text = inputRef.current.value;
+        console.log(text);
+
+        const queryText = `act as a movie recommendation system and suggest some movies for the query: ${text} only gave me name of 5 movies. comma separated`;
+
+        const gptResult = await openai.chat.completions.create({
+            messages: [{ role: 'user', content: queryText }],
+            model: 'gpt-5.2',
+        });
+
+        if(!gptResult.choices){
+            // show error message
+        }
+
+        //console.log(gptResult._request_id);
+        //console.log(gptResult.choices[0].message.content);
+        //console.log(gptResult.choices?.[0]?.message?.content);
+        // result is comma separated so convert it to array
+        const gptResultArray = gptResult.choices?.[0]?.message?.content.split(",");
+        console.log(gptResultArray);
+        //const lastGptSearchObj = { searchContent: text, searchResult: gptResultArray };
+        //dispatch(lastGptSearch(lastGptSearchObj));
+
+        const gptSearchPromiseArray = gptResultArray.map((movieName) => findSearchMovies(movieName));
+        const gptSearchResult = await Promise.all(gptSearchPromiseArray);
+        console.log(gptSearchResult);
+        dispatch(lastGptSearch({searchContent: text, movieNames:gptResultArray, searchResult: gptSearchResult}));
+    }
+
+    return (
+        <div>
+            <form onSubmit={searchEvent}>
+                <input ref={inputRef} type="text" placeholder="Search..." />
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    )
 }
 
 export default GptSearchBar
--- reactjs/flimflix-gpt/src/utils/gptSlice.js
+++ reactjs/flimflix-gpt/src/utils/gptSlice.js
@@ -2,9 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const gptSlice = createSlice({
     name:"gpt",
-    initialState: null,
-    reducers:{}
+    initialState:{
+        searchContent: null,
+        movieNames: null,
+        searchResult: null
+    },
+    reducers:{
+        lastGptSearch:(state, action) => {
+            const { searchContent, movieNames, searchResult } = action.payload;
+            state.searchResult = searchResult;
+            state.searchContent = searchContent;
+            state.movieNames = movieNames;
+        },
+    }
 });
 
 export default gptSlice.reducer;
-export const {} = gptSlice.actions;
+export const { lastGptSearch } = gptSlice.actions;
