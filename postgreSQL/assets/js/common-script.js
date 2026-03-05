925cf9be4da3db928bc4ad3fc0e2c040f7842716
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-03-05 23:34:29 +0530

    postgresql Added

---
+++ postgreSQL/assets/js/common-script.js
@@ -0,0 +1,32 @@
+var editor = CodeMirror.fromTextArea(
+    document.getElementById("editor"),
+    {
+        mode: "javascript",
+        theme: "neo",
+        lineWrapping: true,
+        lineNumbers: false,
+        readOnly: true,
+    }
+);
+
+
+var editors = document.querySelectorAll(".code-editor");
+
+editors.forEach(function (textarea) {
+  CodeMirror.fromTextArea(textarea, {
+    mode: "javascript",
+    theme: "neo",
+    lineWrapping: true,
+    lineNumbers: true,
+    readOnly: true,
+  });
+});
+
+/*
+<textarea id="editor">
+test
+</textarea>
+<textarea class="code-editor">
+test
+</textarea>
+*/
