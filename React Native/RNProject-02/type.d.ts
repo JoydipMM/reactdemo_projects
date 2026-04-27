7eb4697a3a1464955a2db40e9371133eeb8298b9
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-04-27 22:14:29 +0530

    added

--- React Native/RNProject-02/type.d.ts
+++ React Native/RNProject-02/type.d.ts
@@ -11,6 +11,46 @@ declare global {
     icon?: ImageSourcePropType;
     href?: null;
   }
+  interface Subscription {
+    id: string;
+    icon: ImageSourcePropType;
+    name: string;
+    plan?: string;
+    category?: string;
+    paymentMethod?: string;
+    status?: string;
+    startDate?: string;
+    price: number;
+    currency?: string;
+    billing: string;
+    renewalDate?: string;
+    color?: string;
+  }
+
+  interface SubscriptionCardProps extends Omit<Subscription, "id"> {
+    expanded: boolean;
+    onPress: () => void;
+    onCancelPress?: () => void;
+    isCancelling?: boolean;
+  }
+
+  interface UpcomingSubscription {
+    id: string;
+    icon: ImageSourcePropType;
+    name: string;
+    price: number;
+    currency?: string;
+    daysLeft: number;
+  }
+
+  interface UpcomingSubscriptionCardProps extends Omit<
+    UpcomingSubscription,
+    "id"
+  > {}
+
+  interface ListHeadingProps {
+    title: string;
+  }
 }
 
 export { };
