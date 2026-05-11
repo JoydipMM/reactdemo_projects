036182350af62e51ac36d728203f9246dd6e4da1
joydip <joydip@matrixnmedia.com>
2026-05-11 18:41:08 +0530

    added

---
+++ reactjs/flimflix-gpt-v2/src/shared/api/client.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { API_BASE_URL } from "@/shared/utils/constants";
+
+const axiosClient = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_READ_ACCESS_TOKEN}`
+    }
+})
+
+export default axiosClient;
