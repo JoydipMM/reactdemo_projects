73ace1b5333ac205a8636c0c968f5617acb7088e
joydip <joydip@matrixnmedia.com>
2026-05-11 17:05:50 +0530

    added

--- reactjs/flimflix-gpt-v2/src/App.jsx
+++ reactjs/flimflix-gpt-v2/src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      App Component
+      App Component<br/>
+    <h1 class="text-3xl font-bold text-amber-700 underline">
+  Hello world!
+</h1>
     </>
   )
 }
