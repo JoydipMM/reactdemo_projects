b6d95f936893f76b069d517099af74c1e0307557
joydip <joydip@matrixnmedia.com>
2026-05-13 19:40:14 +0530

    added

--- reactjs/flimflix-gpt-v2/src/shared/api/endpoints.ts
+++ reactjs/flimflix-gpt-v2/src/shared/api/endpoints.ts
@@ -1,5 +1,9 @@
 export const ENDPOINTS = {
     USERS: '/users',
     USER: '/users/:id',
-    LOGIN: '/auth/login'
+    LOGIN: '/auth/login',
+    users: {
+    list: "/users",
+        detail: (id: number) => `/users/${id}`,
+    },
 }
