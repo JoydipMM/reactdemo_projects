4e153cc1217d91e3324a95afc37b4b74543ae458
joydip <joydip@matrixnmedia.com>
2026-05-15 19:19:38 +0530

    added

--- reactjs/redux-learn-v2-2/src/app/provider/QueryProvider.jsx
+++ reactjs/redux-learn-v2-2/src/app/provider/QueryProvider.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(); // here we create a new instance of QueryClient not inside function. Because If we create a new instance of QueryClient inside function, it will be recreated every re-render, which is not what we want.
 
 function QueryProvider({ children }) {
   return (
