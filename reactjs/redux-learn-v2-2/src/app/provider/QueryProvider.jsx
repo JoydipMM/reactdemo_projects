14e3af7d029e701c73a4d91c10a2cda98a385c47
joydip <joydip@matrixnmedia.com>
2026-05-15 19:18:11 +0530

    added

--- reactjs/redux-learn-v2-2/src/app/provider/QueryProvider.jsx
+++ reactjs/redux-learn-v2-2/src/app/provider/QueryProvider.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; 
+const queryClient = new QueryClient();
 
 function QueryProvider({ children }) {
-    const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
         {children}
