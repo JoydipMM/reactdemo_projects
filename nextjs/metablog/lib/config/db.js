2bb2be00cc24e4e018e6c70331180153903f3957
joydip <joydip@matrixnmedia.com>
2026-06-04 13:28:49 +0530

    added

--- nextjs/metablog/lib/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose, { connect } from "mongoose";
-
-const connectDB = async () => {
-    await mongoose.connect(process.env.);
-    console.log("DB connected");
-}
-
-export default connectDB;
--- nextjs/metablog/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ["fastly.picsum.photos"], // Add external domains here
-    },
-};
-
-export default nextConfig;
