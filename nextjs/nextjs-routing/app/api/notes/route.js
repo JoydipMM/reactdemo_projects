0675933dddadbae01a0f8cd779adf5a05e4711a3
joydip <joydip@matrixnmedia.com>
2026-06-09 16:23:39 +0530

    model and schema added

---
+++ nextjs/nextjs-routing/app/api/notes/route.js
@@ -0,0 +1,25 @@
+import { connectDB } from "@/shared/lib/db";
+import { Note } from "@/shared/models/Notes";
+
+export async function POST(request){
+
+    // 1. when ever we calling any endpoint we need connect our database first
+    await connectDB();
+
+    // 2. get data from request body
+    const { title, content } = await request.json();
+
+    // 3. import model to the above from schema for save data to database 
+    
+    // 4. save the data to database
+    const note = await Note.create({ title, content });
+    // syntax: await ModelName.create({ key: value });
+
+    // 5. return the saved data to client
+    return Response.json({ 
+        note:note, 
+        status: 201,
+        success: true, 
+        message: "note added successfully", 
+    });
+}
