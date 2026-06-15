ff86cb35fe496132c7e85d419a2c19158bb6fe4a
joydip <joydip@matrixnmedia.com>
2026-06-15 17:53:26 +0530

    added

---
+++ nextjs/nextjs-routing/app/parallal-route/@about/about-inner/default-.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DefaultAboutInner = () => {
+  return (
+    <div>DefaultAboutInner</div>
+  )
+}
+
+export default DefaultAboutInner
---
+++ nextjs/nextjs-routing/app/parallal-route/@about/default-.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DefaultAbout = () => {
+  return (
+    <div>DefaultAbout</div>
+  )
+}
+
+export default DefaultAbout
