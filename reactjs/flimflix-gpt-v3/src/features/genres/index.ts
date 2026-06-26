de8455115f35040e994fd7c1d6b9ba3ec394dc29
joydip <joydip@matrixnmedia.com>
2026-06-26 19:22:38 +0530

    added

--- reactjs/flimflix-gpt-v3/src/features/genres/index.ts
+++ reactjs/flimflix-gpt-v3/src/features/genres/index.ts
@@ -1,3 +1,3 @@
-export type * from "@/features/genres/types/genre.types";
-export  * from "@/features/genres/queries/useGenreQueries";
-export { default as GenreList } from "@/features/genres/sections/GenreList";
+export type * from "./types/genre.types";
+export  * from "./queries/useGenreQueries";
+export { default as GenreList } from "./sections/GenreList";
