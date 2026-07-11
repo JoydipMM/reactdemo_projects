0092863c17f14ed5157cc6030eb287ef13a0110c
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-07-11 21:37:50 +0530

    added

--- nodejs/nodejs-sangam/Basics/src/event-emitter/index.js
+++ nodejs/nodejs-sangam/Basics/src/event-emitter/index.js
@@ -5,9 +5,90 @@ const eventEmitter = require("events");
 const myFirstEmitter = new eventEmitter();
 
 // 03. Register a listener for the event
-myFirstEmitter.on("firstEmittedEvent", () => {
-    console.log("event emitted");
+myFirstEmitter.on("firstEmittedEvent", (data) => {
+    console.log("event emitted", data);
 });
 
 // 04. Emit the event
-myFirstEmitter.emit("firstEmittedEvent");
+//myFirstEmitter.emit("firstEmittedEvent", "passing the data");
+
+
+
+
+// custom event emitter
+class customEmitter extends eventEmitter{
+    constructor(){
+        super();
+        this.greeting = "Hello";
+    }
+
+    // create a method will emit the event
+    greet(name){
+        this.emit('greeting', `${this.greeting} ${name}`);
+    }
+}
+
+const myCustomEmitter = new customEmitter();
+
+myCustomEmitter.on('greeting', (data) => {
+    console.log(data);
+});
+
+//myCustomEmitter.greet("Rahul");
+
+
+
+
+
+
+class Auth extends eventEmitter {
+    login(username) {
+        console.log(`${username} logged in`);
+
+        this.emit("login", username);
+    }
+
+    logout(username) {
+        console.log(`${username} logged out`);
+
+        this.emit("logout", username);
+    }
+}
+
+const auth = new Auth();
+
+auth.on("login", (user) => {
+    console.log(`Welcome ${user}`);
+});
+
+auth.on("logout", (user) => {
+    console.log(`Goodbye ${user}`);
+});
+
+auth.login("Rahul");
+/*
+Rahul logged in
+Welcome Rahul
+*/
+auth.logout("Rahul");
+/*
+Rahul logged out
+Goodbye Rahul
+*/
+
+
+
+const authEventEmitter = new eventEmitter();
+
+authEventEmitter.on("login", (user)=>{
+    console.log(`${user} logged in`);
+    console.log(`Welcome ${user}`);
+})
+
+authEventEmitter.on("logout", (user)=>{
+    console.log(`${user} logged out`);
+    console.log(`Goodbye ${user}`);
+})
+
+authEventEmitter.emit("login", "Raja");
+authEventEmitter.emit("logout", "Raja");
--- nodejs/nodejs-sangam/node-curd-app/app.js
+++ nodejs/nodejs-sangam/node-curd-app/app.js
@@ -1,13 +1,173 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+dotenv = require('dotenv');
+dotenv.config();
 const port = 4000;
+const mongodb_url = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.42ucz5w.mongodb.net/${process.env.MONGODB_DATABASE}`
+
+
+async function connectDB(){
+    try{
+        const connect = await mongoose.connect(mongodb_url);
+        console.log(`${connect.connection.db.databaseName} database connected successfully`);
+    }catch(err){
+        console.log("Connection failed. Error:", err);
+    }finally{
+        await mongoose.connection.close();
+    }
+}
+
+//connectDB();
+
+mongoose.connect(mongodb_url).then(() => {
+    console.log("Database connected successfully");
+}).catch((err) => {
+    console.log("Connection failed. Error:", err);
+})
+
+
+
+const bookSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    author:{
+        type: String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    isActive:{
+        type: Boolean,
+        default: false,
+    }
+}, {timestamps: true});
+
+const Books = mongoose.models.Books || mongoose.model("Books", bookSchema);
+/* Note:
+1. model name always plural and first letter capital.
+2. schema name always singular 
+3. collection name always plural and all in small letter
+*/
+
+async function runQueryExample(){
+    try{
+
+        // create a new book document in database option 01
+        // const NewBook = await Books.create({
+        //     title: "New Book 005",
+        //     author: "New Author",
+        //     price: 600
+        // });
+        // console.log("New Book created: ",NewBook);
+
+        // create a new book document in database option 02
+        /*const book = new Books({
+            title: "New Book 002",
+            author: "New Author 002",
+            price: 300
+        });
+        await book.save();
+        console.log("New Book created: ",book);*/
+
+        // get all books from database
+        // const books = await Books.find({});
+        // console.log("All books: ",books);
+
+        // get not active books from database
+        // const inActiveBooks = await Books.find({isActive: false});
+        // console.log("All inactive books: ",inActiveBooks);
+
+        // get find one: active book from database:
+        // This findOne() method returns the first document that matches the filter criteria.
+        // const activeBook = await Books.findOne({isActive: true});
+        // console.log("First Active book: ",activeBook);
+
+        // find a book with specific book id from database
+        // const findBookByID = await Books.findOne({_id: "6a51fbad99138c2ef01566ec"});
+        // console.log("Book found: ",findBookByID);
+
+        // select some specific fields from database
+        // const getBookswithSpecificProperties = await Books.find().select("title author -_id"); // -_id will exclude _id field
+        // console.log("Book found: ",getBookswithSpecificProperties);
+        /*
+        Result:
+        Book found:  [
+            { title: 'book 001', author: 'JD Bose' },
+            { title: 'New Book', author: 'New Author' },
+            { title: 'New Book 002', author: 'New Author 002' }
+        ]
+        */
+
+        // limited and skip
+        // await Books.find().limit(1).skip(2); > get all user, then show only 1 user after skipping first 2.
+        // const getBookswithSpecificProperties = await Books.find().limit(1).skip(2);
+        // console.log("Book found: ",getBookswithSpecificProperties);
+        /*
+        Result:
+        Book found:  [
+            {
+                _id: new ObjectId('6a51fbad99138c2ef01566ec'),
+                title: 'New Book 002',
+                author: 'New Author 002',
+                price: 300,
+                createdAt: 2026-07-11T08:15:41.116Z,
+                updatedAt: 2026-07-11T08:15:41.116Z,
+                __v: 0,
+                isActive: true
+            }
+        ]
+        */
+
+        // shorting
+        //const shortingBooks = await Books.find().sort({price: -1}); // 1 for ascending and -1 for descending
+        //console.log("All books: ",shortingBooks);
+
+        // count document 
+        // const countDocument = await Books.countDocuments({isActive:true});
+        // console.log(countDocument);
+        // result: 2
+
+        // delete document
+        // const deleteBookID = "6a526685c714cc91ca6cb4dc"
+        // const findBook = await Books.findById({_id:deleteBookID});
+        // if(!findBook){
+        //     console.log("Book not found!!!")
+        // }else{
+        //     const deleteBook = await Books.findByIdAndDelete({_id:deleteBookID});
+        //     console.log(deleteBook, "Book deleted successfully!!!");
+        // }
+
+        // updateBook
+        // const updateBook = await Books.findByIdAndUpdate(
+        //     {_id:"6a526666f7968ddf3615a65d"},
+        //     { 
+        //         $set: { price: 1500 },
+        //         //$push: { tags: "New Tag" } // push something new
+        //     },
+        //     { returnDocument : true} // new : true >>> this new is deprecated instead of "new" use "returnDocument"
+        // );
+        // console.log(updateBook, "book data updated!!!")
+        
+
+    }catch(error){
+        console.log("Error: ",error);
+    }finally{
+        await mongoose.connection.close();
+    }
+}
+
+runQueryExample();
+
+
+ 
 
 const data = [
-    {id:1, name:"name1"},
-    {id:2, name:"name2"},
-    {id:3, name:"name3"},
-    {id:4, name:"name4"},
-    {id:5, name:"name5"},
+    {id:1, name:"Product 1"},
 ]
 
 const demoMiddleware = (req, res, next) => {
@@ -33,21 +193,30 @@ app.get("/products/:id", (req,res)=>{
     if(!filteredProduct){
         return res.status(404).json({message:"Product not found"});
     }else{
-        return res.json({title:"Products", data:filteredProduct});  
+        return res.status(200).json({title:"Products", data:filteredProduct});  
     }
 });
 app.post("/add", (req,res)=>{
-  const newProduct = {
-    id: data.length + 1,
-    //name: req.body.name // real projct it will be req.body
-    name: `name${data.length + 1}`
-  } 
-  data.push(newProduct);
-  res.json({
+//   const newProduct = {
+//     //id: data.length + 1,
+//     id:`${Math.floor(Math.random() * 1000)}`,
+//     //name: req.body.name // real projct it will be req.body
+//     name: `name${Math.floor(Math.random() * 1000)}`
+//   } 
+//   data.push(newProduct);
+
+    console.log(req.body);
+
+    // const newProduct = Books.create({
+    //     title: req.body.title,
+    //     author: req.body.author,
+    //     price: req.body.price
+    // });
+
+    res.status(200).json({
     data:newProduct,
     message: "New product added successfully"
-  })
-
+    });
 });
 app.put("/update/:id", (req,res)=>{
     const updatedProduct = data.find((product) => product.id == req.params.id);
@@ -55,7 +224,7 @@ app.put("/update/:id", (req,res)=>{
         return res.status(404).json({message:"Product not found"});
     }else{
          updatedProduct.name =  `${updatedProduct.name} updated`;
-         return res.json({ status: 202, message:"Products updated", data: updatedProduct});
+         return res.status(202).json({message:"Products updated", data: updatedProduct});
     }
 });
 app.delete("/delete/:id", (req,res)=>{
@@ -64,7 +233,7 @@ app.delete("/delete/:id", (req,res)=>{
         return res.status(404).json({message:"Product not found"});
     }else{
         data.splice(data.indexOf(deletedProduct), 1);
-        return res.json({title:"Products", data});  
+        return res.status(202).json({title:"Products", data});  
     }
 });
 
