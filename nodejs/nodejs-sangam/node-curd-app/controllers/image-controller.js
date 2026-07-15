394cf9b52039c0abfda3585016b3156f7811bc21
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-07-15 10:42:12 +0530

    added

--- nodejs/nodejs-sangam/node-curd-app/controllers/image-controller.js
+++ nodejs/nodejs-sangam/node-curd-app/controllers/image-controller.js
@@ -1,5 +1,6 @@
 const Image = require("../models/Image");
 const { uploadToCloud } = require("../helpers/cloudenaryHelper");
+const cloudenary = require("../config/cloudenary");
 const fs = require("fs");
 
 const uploadImageController = async (req, res) =>{
@@ -48,4 +49,53 @@ const fetchImagesController = async (req, res) =>{
 
 }
 
-module.exports = { uploadImageController, fetchImagesController };
+const fetchImagesByUserController = async (req, res) =>{
+    try{
+        const loggedUserId = req.userInfo.userId;
+        console.log(loggedUserId)
+        const getImages = await Image.find({uploaded_by: loggedUserId}).populate("uploaded_by" , "email username");
+        if(getImages.length > 0){
+            return res.status(200).json({ success:true, message: "Images found", images: getImages, total: getImages.length, limit: 5, skip: 0 });
+        }
+    }catch(error){
+        return res.status(500).json({ success:false, message: `Error:: ${error.message}` || "Somthing went wrong" });
+    }
+
+}
+
+
+const deleteImageController = async (req, res) =>{
+    try{
+        // 1. get image id from req.params.imageId and user id from req.userInfo
+        const deletedImageId = req.params.imageId;
+        const loggedUserId = req.userInfo.userId;
+
+        // 2. find image details from params id
+        const isValidateImage = await Image.findById(deletedImageId);
+
+        // 2. validate the image id is valid or not
+        if(!isValidateImage){
+            return res.status(404).json({ success:false, message: "Image not found" });
+        }
+
+        // 3. validate the user who is deleting the image is valid or not
+        if(!isValidateImage.uploaded_by.toString() === loggedUserId){
+            return res.status(403).json({ success:false, message: "You are not authorized to delete this image" });
+        }
+
+        // 4. delete the image from cloudinary
+        // 5. delete the image from mongodb database
+        const deleteImageFromCloudenary = await cloudenary.uploader.destroy(isValidateImage.public_id);
+
+        if(deleteImageFromCloudenary.result === "ok"){
+            const deletedImage = await Image.findByIdAndDelete(deletedImageId);
+            return res.status(200).json({ success:true, message: "Image deleted successfully", image: deletedImage });
+        }
+
+
+    }catch(error){
+        res.status(500).json({ success:false, message: `Error:: ${error.message}` || "Somthing went wrong" });
+    }
+}
+
+module.exports = { uploadImageController, fetchImagesController, fetchImagesByUserController, deleteImageController };
--- nodejs/nodejs-sangam/node-curd-app/routes/image-route.js
+++ nodejs/nodejs-sangam/node-curd-app/routes/image-route.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const authMiddleware = require('../middleware/auth-middleware');
 const adminMiddleware = require('../middleware/admin-middleware');
 const uplodMiddleware = require('../middleware/upload-middleware');
-const { uploadImageController, fetchImagesController } = require('../controllers/image-controller');
+const { uploadImageController, fetchImagesController, fetchImagesByUserController, deleteImageController } = require('../controllers/image-controller');
 
 // upload image
 router.post("/upload", authMiddleware, adminMiddleware, uplodMiddleware.single("image"), uploadImageController);
-router.get("/uploaded-images", authMiddleware, fetchImagesController);
+router.get("/uploaded-images", fetchImagesController);
+router.get("/uploaded-user-images", authMiddleware, adminMiddleware, fetchImagesByUserController);
+router.post("/delete/:imageId", authMiddleware, adminMiddleware, deleteImageController);
 
 // get all uploaded images
 
