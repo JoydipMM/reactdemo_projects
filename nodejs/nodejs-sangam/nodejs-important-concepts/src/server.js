c717543e4275bb43d4e53294c1bad0dfd6ad2737
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-08-01 11:06:56 +0530

    pub/sub added

--- nodejs/nodejs-sangam/nodejs-important-concepts/src/server.js
+++ nodejs/nodejs-sangam/nodejs-important-concepts/src/server.js
@@ -23,22 +23,41 @@ async function testConnectionRedis (){
         await redisClient.connect(); // connect to redis
         console.log("Redis Client connected");
 
-        // hSet, hGet, hGetAll, hDel
-        await redisClient.hSet("product:1", {
-            name: "Laptop",
-            price: 1000,
-            quantity: 10
+        // pub/sub -----------------------
+
+        // 01. we create a duplicate client for subscriber
+        const subscriber = redisClient.duplicate();
+
+        // 02. connect redis server for subscriber
+        await subscriber.connect();
+
+        // 03. to subscribe to a channel, we can use the subscribe method of the subscriber client. The subscribe method takes two arguments: the name of the channel and a callback function that will be called whenever a message is published to that channel. The callback function takes two arguments: the name of the channel and the message that was published.
+        // syntax: await subscriber.subscribe("channel-name", (channel, message) => {});
+        await subscriber.subscribe("news-channel", (message, channel) =>{
+            console.log(`Received message from ${channel}: ${message}`);
         })
-        const getProductSpeficData = await redisClient.hGet("product:1", "name");
-        console.log(getProductSpeficData); // Result: "Laptop"
-
-        getAllProductDetails = await redisClient.hGetAll("product:1");
-        console.log(getAllProductDetails); // Result: { name: 'Laptop', price: '1000', quantity: '10' }
 
 
-        await redisClient.hDel("product:1", "name");
-        const updatedProductDetails = await redisClient.hGetAll("product:1");
-        console.log(updatedProductDetails);
+        // 04. publish a message to the channel using the publish method of the redis client. The publish method takes two arguments: the name of the channel and the message to be published.
+        // syntax: await redisClient.publish("channel-name", "message");
+        await redisClient.publish("news-channel", "Hello, this is a test message from the publisher!"); 
+        await redisClient.publish("news-channel", "This is another test message from the publisher!"); 
+
+
+        // 05. we need to wait for a few seconds to make sure that the subscriber has received the messages before we close the connection. We can use setTimeout to wait for a few seconds.
+        setTimeout(async () => {
+            await subscriber.unsubscribe("news-channel"); // unsubscribe from the channel
+            await subscriber.quit(); // close the connection for subscriber
+            console.log("Subscriber disconnected");
+        }, 5000);
+
+        // Result:
+        /*
+        Redis Client connected
+        Received message from news-channel: Hello, this is a test message from the publisher!
+        Received message from news-channel: This is another test message from the publisher!
+        Subscriber disconnected
+        */
 
 
 
