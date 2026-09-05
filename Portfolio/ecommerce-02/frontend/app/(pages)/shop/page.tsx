fba1862396a83b56a4b9e333484f884e56816539
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-09-05 10:10:46 +0530

    added

--- Portfolio/ecommerce-02/frontend/app/(pages)/shop/page.tsx
+++ Portfolio/ecommerce-02/frontend/app/(pages)/shop/page.tsx
@@ -6,9 +6,26 @@ export default function ShopPage() {
     <div>
       <section className="bg-gray-50 py-16">
         <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col sm:flex-row gap-5 items-center mb-10">
+          <div className="flex flex-col sm:flex-row gap-5 items-start mb-10">
             <FilterOption/>
+
+            <div className="flex-1">
+              <div className="mb-4 flex items-center justify-between">
+                <h2 className="text-2xl font-semibold text-primary">Shop</h2>
+                <select className="rounded-lg border border-border bg-background p-3 text-sm outline-none focus:border-primary">
+                  <option value="low-high">Sort By: Price (Low to High)</option>
+                  <option value="high-low">Sort By: Price (High to Low)</option>
+                  <option value="newest">Sort By: Newest</option>
+                  <option value="oldest">Sort By: Oldest</option>
+                </select>
+              </div>
+            </div>
+
+
+
           </div>
+
+          
 
           {/* <SectionHeading title="Featured Products" subtitle="Check out our featured products." /> */}
 
--- Portfolio/ecommerce-02/frontend/app/components/FilterOption.tsx
+++ Portfolio/ecommerce-02/frontend/app/components/FilterOption.tsx
@@ -7,30 +7,53 @@ export default function FilterOption() {
   return (
     <div className="w-full sm:max-w-60 sm:min-w-60">
 
-    <button type="button" className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 transition hover:border-gray-900 hover:text-white hover:bg-gray-900 cursor-pointer mb-4" onClick={() => setShowMobFilter((prev)=> !prev)}> Filter <span className={`transition-transform duration-300 sm:hidden ${showMobFilter? 'rotate-180' : ''}`}><FaArrowDown /></span></button>
+    <button type="button" className="inline-flex items-center gap-2 rounded-lg border border-gray-300 bg-white px-4 py-2.5 text-sm font-medium text-gray-700 transition hover:border-gray-900 hover:text-white hover:bg-gray-900 cursor-pointer mb-4 sm:hidden" onClick={() => setShowMobFilter((prev)=> !prev)}> Filter <span className={`transition-transform duration-300 sm:hidden ${showMobFilter? 'rotate-180' : ''}`}><FaArrowDown /></span></button>
 
-    <div className={`space-y-6 sm:block ${showMobFilter ? 'block' : 'hidden'}`}>
-        <div className="rounded-xl border border-gray-600 p-5">
-            <h3 className="font-semibold text-black mb-2">CATEGORIES</h3>
+      <div className={`space-y-6 sm:block ${showMobFilter ? 'block' : 'hidden'}`}>
 
-            <div className="w-full space-y-2 text-sm text-gray-600">
-              <label className="flex items-center">
-                <input type="checkbox" className="mr-2" />
-                <span>Women</span>
-              </label>
-              <label className="flex items-center">
-                <input type="checkbox" className="mr-2" />
-                <span>Men</span>
-              </label>
-              <label className="flex items-center">
-                <input type="checkbox" className="mr-2" />
-                <span>Children</span>
-              </label>
-            </div>
-
-
+        <div className="rounded-xl border border-gray-300 p-5">
+          <h3 className="mb-4 text-sm font-semibold tracking-wide">CATEGORIES</h3>
+          <div className="space-y-3 text-sm text-gray-600">
+            <label className="flex items-center gap-3">
+              <input type="checkbox" className="mr-2" />
+              <span>Women</span>
+            </label>
+            <label className="flex items-center gap-3">
+              <input type="checkbox" className="mr-2" />
+              <span>Men</span>
+            </label>
+            <label className="flex items-center gap-3">
+              <input type="checkbox" className="mr-2" />
+              <span>Children</span>
+            </label>
+          </div>
         </div>
-    </div>
+
+        <div className="rounded-xl border border-gray-300 p-5">
+          <h3 className="mb-4 text-sm font-semibold tracking-wide">PRODUCT TYPE</h3>
+          <div className="space-y-3 text-sm text-gray-600">
+            <label className="flex items-center gap-3">
+              <input type="checkbox" className="mr-2" />
+              <span>T-Shirts</span>
+            </label>
+            <label className="flex items-center gap-3">
+              <input type="checkbox" className="mr-2" />
+              <span>Shirts</span>
+            </label>
+            <label className="flex items-center gap-3">
+              <input type="checkbox" className="mr-2" />
+              <span>Hoodies</span>
+            </label>
+            <label className="flex items-center gap-3">
+              <input type="checkbox" className="mr-2" />
+              <span>Jackets</span>
+            </label>
+          </div>
+        </div>
+
+            
+
+      </div>
 
     </div>
   )
