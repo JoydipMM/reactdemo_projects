824e693ab67f72d76bb41b0ab1e8eb9aa188c34a
Joydip Sarkar <c_joydip.sarkar@infinitylearn.com>
2026-09-06 10:19:51 +0530

    added

--- Portfolio/ecommerce-02/frontend/app/components/index.ts
+++ Portfolio/ecommerce-02/frontend/app/components/index.ts
@@ -1,4 +1,5 @@
 export { default as SectionHeading } from './ui/SectionHeading';
-export { default as FilterOption } from './FilterOption';
+export { default as FilterOption } from './ui/FilterOption';
 export { default as Button } from './ui/Button';
 export { default as BreadCrumbs } from './ui/BreadCrumbs';
+export { default as Input } from './ui/Input';
